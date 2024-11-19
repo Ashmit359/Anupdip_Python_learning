8926fe296599278b28d1a80acb1c0773964709ea
Ashmit359 <ashmit.sinha359@gmail.com>
2024-11-19 12:31:22 +0530

    add d5 pdf and code, today topic is for loop

---
+++ d5_for_loop_range.py
@@ -0,0 +1,27 @@
+# print the following sequence
+# 4,-8,12,-16,20,-24,..........,-816,820
+# term = 4
+# while(term <= 820):
+#     if (term % 8 == 0):
+#         print(- term, end=",")
+#     else:
+#         print(term,end=",")
+#     term = term + 4        
+
+
+# FOR LOOP
+# exm-1
+# for x in [80,70,20,50,620]:
+#     print(x,end=",")
+
+# exm-2
+# for m in [4,8,5,6,2]:
+#     print((m**2), end=",")
+
+# exm -3
+sum = 0
+for x in [40,50,60,70]:
+    print(x ,end = ",")
+    sum = sum + x,
+
+print("in sum", sum)    
