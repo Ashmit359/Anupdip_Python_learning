5c41f23732373f9a4a797a6cb5b4984e2a4c2107
Ashmit359 <ashmit.sinha359@gmail.com>
2024-11-20 19:17:59 +0530

    add some question and update files with question

--- d4_if_elif_else_&_loops.py
+++ d4_if_elif_else_&_loops.py
@@ -1,57 +1,77 @@
-#1 write a program to find out the greatest number among four number 
-# take input form user 
-num1 = int (input("enter your first number : "))
-num2 = int (input("enter your second number : "))
-num3 = int (input("enter your third number : "))
-num4 = int (input("enter your four number : "))
-# check greatest number 
-if   (num1 == num2 and num1 == num3 and num1 == num4):
-    print("all number are equal ")
-elif (num1 >= num2 and num1 >= num3 and num1 >= num4):
-    print(num1, "num1 is greatest number ")
-elif (num2 >= num1 and num2 >= num3 and num2 >= num4):
-    print(num2, "num2 is greatest number ")
-elif (num3 >= num1 and num3 >= num2 and num3 >= num4):
-    print(num3, "num3 is greatest number ") 
-else:
-    print(num4, "num4 is greastest number "),
+# #1 write a program to find out the greatest number among four number 
+# # take input form user 
+# num1 = int (input("enter your first number : "))
+# num2 = int (input("enter your second number : "))
+# num3 = int (input("enter your third number : "))
+# num4 = int (input("enter your four number : "))
+# # check greatest number 
+# if   (num1 == num2 and num1 == num3 and num1 == num4):
+#     print("all number are equal ")
+# elif (num1 >= num2 and num1 >= num3 and num1 >= num4):
+#     print(num1, "num1 is greatest number ")
+# elif (num2 >= num1 and num2 >= num3 and num2 >= num4):
+#     print(num2, "num2 is greatest number ")
+# elif (num3 >= num1 and num3 >= num2 and num3 >= num4):
+#     print(num3, "num3 is greatest number ") 
+# else:
+#     print(num4, "num4 is greastest number "),
   
 
 
-#2 write a program to input any three number and arrange them in ascending order
-# take input from user 
-num1 = int (input("enter first number : "))
-num2 = int (input("enter second number : "))
-num3 = int (input("enter third number : "))
-# check greatest and smallest number 
-if(num1 == num2 and num1 == num3):
-    print("all numbers are equal ")
-elif(num1 <= num2 and num1 <= num3):
-    print("ascending order :- ", num1,num2,num3)
-elif(num2 <= num1 and num2 <= num3):
-    print("ascending order :- ", num2, num1, num3)
+# #2 write a program to input any three number and arrange them in ascending order
+# # take input from user 
+# num1 = int (input("enter first number : "))
+# num2 = int (input("enter second number : "))
+# num3 = int (input("enter third number : "))
+# # check greatest and smallest number 
+# if(num1 == num2 and num1 == num3):
+#     print("all numbers are equal ")
+# elif(num1 <= num2 and num1 <= num3):
+#     print("ascending order :- ", num1,num2,num3)
+# elif(num2 <= num1 and num2 <= num3):
+#     print("ascending order :- ", num2, num1, num3)
+# else:
+#     print("ascendig order :- ", num3, num1, num2),
+
+
+
+# #3 while loop 
+# x = 10 
+# while (x <= 30):
+#     print(x)
+#     x = x + 6,
+
+
+# #4 while loop 
+# m = 10
+# while(m <= 40):
+#     print(m,end=",")
+#     m=m+2
+
+# # print the following sequence
+# # 5,9,13,17,21........,205,209
+
+# term = 5
+# while(term <= 209):
+#     print(term, end=",")
+#     term = term + 4
+
+
+# take percentage input from user and base on  percentage value you have to print 
+# that student is in distinction (Percentage>70) /first class(Percentage>60)/
+# sencond class (percentage >50)/third class (percentage >35)/fail(percentage is less than 35)
+
+# Taking percentage input from the user
+marks = int(input("Enter your percentage: "))
+
+# Checking the conditions to determine the class
+if marks > 70:
+    print("Student passed with distinction.")
+elif marks > 60 and marks <= 70:
+    print("Student passed with first class.")
+elif marks > 50 and marks <= 60:
+    print("Student passed with second class.")
+elif marks > 35 and marks <= 50:
+    print("Student passed with third class.")
 else:
-    print("ascendig order :- ", num3, num1, num2),
-
-
-
-#3 while loop 
-x = 10 
-while (x <= 30):
-    print(x)
-    x = x + 6,
-
-
-#4 while loop 
-m = 10
-while(m <= 40):
-    print(m,end=",")
-    m=m+2
-
-# print the following sequence
-# 5,9,13,17,21........,205,209
-
-term = 5
-while(term <= 209):
-    print(term, end=",")
-    term = term + 4
+    print("Student failed.")
--- d5_for_loop_range.py
+++ d5_for_loop_range.py
@@ -1,5 +1,5 @@
-# print the following sequence
-# 4,-8,12,-16,20,-24,..........,-816,820
+# # print the following sequence
+# # 4,-8,12,-16,20,-24,..........,-816,820
 # term = 4
 # while(term <= 820):
 #     if (term % 8 == 0):
@@ -9,19 +9,35 @@
 #     term = term + 4        
 
 
-# FOR LOOP
-# exm-1
+# # FOR LOOP
+
+# # exm-1
 # for x in [80,70,20,50,620]:
 #     print(x,end=",")
 
-# exm-2
+# # exm-2
 # for m in [4,8,5,6,2]:
 #     print((m**2), end=",")
 
-# exm -3
-sum = 0
-for x in [40,50,60,70]:
-    print(x ,end = ",")
-    sum = sum + x,
+# # exm -3
+# sum = 0
+# for x in [40,50,60,70]:
+#     print(x ,end = ",")
+#     sum = sum + x
+# print("in sum", sum)    
 
-print("in sum", sum)    
+
+# RANGE
+
+#1 write a program to print first 20 natural number
+# print ("first 20 natural number : ")
+# for x in range (1,21):
+#     print(x,end=" , ")
+
+# write a program to print first 30 even natural number .also print their sum
+sum = 0
+for x in range (1,31):
+    if(x % 2 == 0 ):
+        print("first even natural number ", x)
+        sum = sum + (x * 2)
+print("in sum = ", sum)        
